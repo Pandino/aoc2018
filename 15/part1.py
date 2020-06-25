313d135062dc25fef4f6f4deeac3eeb57592dbff
Giorgio Ceolin <giorgio.ceolin@crealogix.com>
2020-06-25 18:19:15 +0200

    Day 15 part1 optimized

--- 15/part1.py
+++ 15/part1.py
@@ -7,21 +7,15 @@ from collections import defaultdict
 reading_directions = ((0, -1),(-1, 0),(1, 0),(0, 1))
 reverse = itemgetter(1, 0)
 
-class ReadingCoords():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __lt__(self, other):
-        return (self.y, self.x) < (other.y, other)
-
 class Actor():
     def __init__(self, actor_type, position):
         self.type = actor_type
+        self.origin = position
         self.position = position
         self.hp = 200
         self.ap = 3
         self.alive = True
+        
     
     def __lt__(self, other):
         if type(self) != type(other):
@@ -64,20 +58,24 @@ class Actor():
         # Create a set with the updated position of all obstacles (walls and actors) 
         obstacles = frozenset(dungeon.dungeon.union(actor.position for actor in dungeon.get_actors()))
         # Find all possible target positions around an enemy that are not an obstacle
-        enemies = set(actor for actor in dungeon.get_actors() if actor.type != self.type)
+        enemies = frozenset(actor for actor in dungeon.get_actors() if actor.type != self.type)
         if enemies:
-            targets = set(target for actor in enemies for target in actor.adjacent() if target not in obstacles)
+            targets = frozenset(target for actor in enemies for target in actor.adjacent() if target not in obstacles)
             if targets:
                 # Calculate all the shortest paths from the actor to the target positions. sorting them by distance and reading order
-                distances = ((shortest_path(self.position, target, obstacles), target) for target in targets)
-                valid_distances = list(distance for distance in distances if distance[0] is not None)
-                if valid_distances:
-                    target = sorted(valid_distances, key=lambda x: (x[0], reverse(x[1])))[0][1]
-                    # Choose the step with the shortest path
-                    distances = ((shortest_path(move, target, obstacles), move) for move in self.adjacent() if move not in obstacles)
-                    valid_distances = (distance for distance in distances if distance[0] is not None)
-                    move = sorted(valid_distances, key=lambda x: (x[0], reverse(x[1])))[0][1]
+                # distances = ((shortest_path(self.position, target, obstacles), target) for target in targets)
+                # valid_distances = list(distance for distance in distances if distance[0] is not None)
+                # if valid_distances:
+                #     target = sorted(valid_distances, key=lambda x: (x[0], reverse(x[1])))[0][1]
+                #     # Choose the step with the shortest path
+                #     distances = ((shortest_path(move, target, obstacles), move) for move in self.adjacent() if move not in obstacles)
+                #     valid_distances = (distance for distance in distances if distance[0] is not None)
+                #     move = sorted(valid_distances, key=lambda x: (x[0], reverse(x[1])))[0][1]
+                #     self.position = move
+                move = breath_first(self.position, targets, obstacles)
+                if move:
                     self.position = move
+
             self.attack(dungeon)
             return True
 
@@ -134,11 +132,7 @@ class Dungeon():
         return len(classes) == 1
 
 @lru_cache
-def shortest_path(a, b, obstacles):
-    '''Find shortest path between a and b. Returns the steps needed.'''
-    def distance(a, b):
-        return abs(b[0] - a[0]) + abs(b[1] - b[0])
-
+def breath_first(a, targets, obstacles):
     def next_points(point):
         x, y = point
         yield from ((x+dx, y+dy) for dx, dy in reading_directions if (x+dx, y+dy) not in obstacles)
@@ -146,26 +140,35 @@ def shortest_path(a, b, obstacles):
     frontier = []
     come_from = {}
     cost_to = {}
-    heappush(frontier, (0, a))
-    come_from[a] = None
-    cost_to[a] = 0
+    current = a
+    come_from[current] = None
+    cost_to[current] = 0
+    for source in next_points(current):
+        cost_to[source] = 1
+        come_from[source] = (current, reverse(source))
+        priority = (1, *reverse(source))
+        heappush(frontier, (priority, reverse(source), source))
 
     while len(frontier) > 0:
-        current = heappop(frontier)[1]
+        _, start, current = heappop(frontier)
 
-        if current == b:
+        if current in targets:
             break
 
         for next_point in next_points(current):
             new_cost = cost_to[current] + 1
-            if next_point not in cost_to or new_cost < cost_to[next_point]:
+            if next_point not in cost_to or new_cost < cost_to[next_point] or (new_cost == cost_to[next_point] and start < come_from[next_point][1]):
                 cost_to[next_point] = new_cost
-                priority = new_cost + distance(next_point, b)
-                heappush(frontier, (priority, next_point))
-                come_from[next_point] = current
+                priority = (new_cost, *reverse(next_point))
+                heappush(frontier, (priority, start, next_point))
+                come_from[next_point] = (current, start)
     else:
         return None
-    return cost_to[b]
+    
+    while True:
+        if come_from[current][0] == a:
+            return current
+        current = come_from[current][0]
 
 def test_suite():
     files = [('15/sample2', 27730),
@@ -173,7 +176,8 @@ def test_suite():
             ('15/sample4', 39514), 
             ('15/sample5', 27755), 
             ('15/sample6', 28944), 
-            ('15/sample7', 18740)]
+            ('15/sample7', 18740),
+            ('15/input', 191575)]
 
     for testname, score in files:
         with open(testname) as f:
@@ -191,23 +195,23 @@ def test_suite():
 if __name__ == "__main__":
     term = Terminal()
     test_suite()    
-    with open('15/input') as f:
-        dungeon = Dungeon(f.readlines())
-    with term.cbreak():
-        dungeon.render(term)
-        term.inkey(timeout=0.05)
-        turns = 0
-        while not dungeon.win():
-            full_turn = dungeon.update()
-            if full_turn:
-                turns += 1
-            #dungeon.render(term)
-            #print(turns)
-            #term.inkey(timeout=None)
+    # with open('15/input') as f:
+    #     dungeon = Dungeon(f.readlines())
+    # with term.cbreak():
+    #     dungeon.render(term)
+    #     term.inkey(timeout=0.05)
+    #     turns = 0
+    #     while not dungeon.win():
+    #         full_turn = dungeon.update()
+    #         if full_turn:
+    #             turns += 1
+    #         dungeon.render(term)
+    #         print(turns)
+    #         term.inkey(timeout=None)
                 
-        print(f'turns: {turns}')
-        score = turns * sum(actor.hp for actor in dungeon.get_actors())
-        print(f'Final score = {score}')
+    #     print(f'turns: {turns}')
+    #     score = turns * sum(actor.hp for actor in dungeon.get_actors())
+    #     print(f'Final score = {score}')
         
         
     
